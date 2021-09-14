abbd630399806bbf6be0a11c569e91361b2e23ae
anmho <andyminhtuanho@gmail.com>
2021-09-14 01:52:27 -0700

    'Added landscape orientation rendering'

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -12,8 +12,10 @@ public class Movie {
     String posterPath;
     String title;
     String overview;
+    String backdropPath;
 
     public Movie(JSONObject jsonObject) throws JSONException {
+        backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
@@ -32,7 +34,7 @@ public class Movie {
     }
 
     public String getPosterPath() {
-        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath); // should fetch sizes
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath); // should fetch sizes // @TODO use get config to get base_url @TODO get image size
     }
 
     public String getTitle() {
@@ -41,6 +43,10 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
     }
 }
 
